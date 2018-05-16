b5217fa97d1cbd913dc958940044fe641fc60f4a
Arun Wilson <cyberinfoscripter@gmail.com>
2018-05-16 19:10:48 +0530

    Push Notifications Utiltiy Componenet ( No used, and package not installed )

---
+++ src/components/utility/pushNotifications.js
@@ -0,0 +1,10 @@
+import React,{ Component } from 'react';
+import PushNotification from 'react-native-push-notification';
+
+export default class PushNotification extends Component {
+
+    render(){
+        return null;
+    }
+
+}
